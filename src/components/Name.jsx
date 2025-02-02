e40e8c81b761577c324c71cc58bbfd06cfcb83dc
omarakamal <omaraakamal@gmail.com>
2025-02-02 20:21:31 +0300

    updated code

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -2,9 +2,31 @@ import "../Name.css"
 function Name(){
     let myName = "Omar"
 
+    const clickAlert = ()=>{
+        alert("ALERT CLICKED")
+    }
+
+
+    // function clickAlert(){
+      
+    // }
+
+    let student = {
+        name: "Sayed",
+        course:"SEB",
+        city: "Manama"
+    }
+
+    let {name,city} = student
+
+    console.log(`Hello my Name is ${name} and I live in ${city
+    }`)
+
+    console.log(myName.toUpperCase())
     return(
         <div>
-            <h2 id="my-name">{myName}</h2>
+            <h2 id="my-name">{myName.toUpperCase()}</h2>
+            <button onClick={clickAlert}>Click For Alert</button>
         </div>
     )
 }
